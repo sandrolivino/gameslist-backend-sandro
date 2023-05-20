62a5f1484dfa7c7790695a0064a88d0928052deb
Sandro Livino <sandro.livino@gmail.com>
2023-05-20 13:26:49 -0300

    D1 - Commit inicial: projeto base

---
+++ src/test/java/br/com/personal/gameslistbackendsandro/GameslistBackendStartupTests.java
@@ -0,0 +1,13 @@
+package br.com.personal.gameslistbackendsandro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameslistBackendStartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
